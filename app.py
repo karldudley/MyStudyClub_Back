dfbfbdb82192a60d186d65df8882ef154222edd5
karl dudley <karldudley@gmail.com>
2022-10-27 19:17:22 +0100

    change db

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://knonbgucbylgdb:91620f58ea09dd7d85b9d24e4b7a26372ea08ee1bede0e8e3bbb3bfc139ec5fc@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d57frogopfmo03' # heroku postgres db
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db' # flask sqlite db
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://knonbgucbylgdb:91620f58ea09dd7d85b9d24e4b7a26372ea08ee1bede0e8e3bbb3bfc139ec5fc@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d57frogopfmo03' # heroku postgres db
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialise the db
 db = SQLAlchemy(app)
@@ -37,20 +38,25 @@ def my_profile():
     print(response_body)
     return response_body
 
-@app.route('/testdb')
+@app.route('/testdb', methods=["POST", "GET"])
 def testdb():
-    # create random instance of User class
-    new_user = User(full_name="karl", user_name="karldudley", email="karl@example.com", password="test123")
+    if request.method == "POST":
+        # create random instance of User class
+        new_user = User(full_name="karl", user_name="karldudley", email="karl@example.com", password="test123")
 
-    # store random string and url in db
-    try:
-        db.session.add(new_user)
-        db.session.commit()
-        return "Correctly added to db"
-        # user_table = User.query.all()
-        # return user_table
-    except:
-        return "There was an error adding to the db"
+        # store random string and url in db
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return "Correctly added to db"
+            # user_table = User.query.all()
+            # return user_table
+        except:
+            return "There was an error adding to the db"
+    else:
+        users = User.query.order_by(User.created_at)
+        print(users[0].full_name)
+        return "hey"
     
 
 if __name__ == '__main__':
