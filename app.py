330e15743a0db0c22a353d1d96b4d4f498bde59d
karl dudley <karldudley@gmail.com>
2022-10-27 17:10:42 +0100

    add first class

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lkounzrgikkdge:10b8b7b4da003cc528748ffd5dbcbe9987af8f7526a5a766be2a77ad097c194d@ec2-52-23-131-232.compute-1.amazonaws.com:5432/d8lp0toh40kf2b' # heroku postgres db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialise the db
+db = SQLAlchemy(app)
+
+# Create user model
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(50), nullable=False)
+    user_name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    #create a function to return a string when we add something
+    def __repr__(self):
+        return '<User %r>' % self.id
 
 @app.route('/')
 def index():
