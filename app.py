eed1b6217aebfbe30fffa64bd8739d96996f35ae
karl dudley <karldudley@gmail.com>
2022-10-27 17:21:46 +0100

    test db

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(50), nullable=False)
-    user_name = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
+    user_name = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(20), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -36,6 +36,14 @@ def my_profile():
     }
     print(response_body)
     return response_body
+
+@app.route('/testdb')
+def testdb():
+    user_table = User.query.all()
+    # for url in table:
+    #     print(url.url)
+    # table = "test"
+    return user_table
 
 if __name__ == '__main__':
     app.run(debug = True)   # pragma: no cover
