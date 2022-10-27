3accc2e9876c9171cb3a5fc0033e4637b3f595b4
karl dudley <karldudley@gmail.com>
2022-10-27 16:52:26 +0100

    prep for first deployment

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lkounzrgikkdge:10b8b7b4da003cc528748ffd5dbcbe9987af8f7526a5a766be2a77ad097c194d@ec2-52-23-131-232.compute-1.amazonaws.com:5432/d8lp0toh40kf2b' # heroku postgres db
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+@app.route('/')
+def index():
+    return "Welcome to the myStudyClub Server!"
+
 @app.route('/profile')
 def my_profile():
     response_body = {
