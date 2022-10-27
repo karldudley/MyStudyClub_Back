187e526f62d9a27cced8264f5cca402007b0137a
karl dudley <karldudley@gmail.com>
2022-10-27 19:56:41 +0100

    change db back

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db' # flask sqlite db
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://knonbgucbylgdb:91620f58ea09dd7d85b9d24e4b7a26372ea08ee1bede0e8e3bbb3bfc139ec5fc@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d57frogopfmo03' # heroku postgres db
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student.db' # flask sqlite db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://knonbgucbylgdb:91620f58ea09dd7d85b9d24e4b7a26372ea08ee1bede0e8e3bbb3bfc139ec5fc@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d57frogopfmo03' # heroku postgres db
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialise the db
 db = SQLAlchemy(app)
 
-# Create user model
-class User(db.Model):
-    __tablename__ = 'user'
+# Create Student model
+class Student(db.Model):
+    __tablename__ = 'student'
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(50), nullable=False)
     user_name = db.Column(db.String(20), nullable=False)
@@ -23,7 +23,7 @@ class User(db.Model):
 
     #create a function to return a string when we add something
     def __repr__(self):
-        return '<User %r>' % self.id
+        return '<Student %r>' % self.id
 
 @app.route('/')
 def index():
@@ -41,21 +41,22 @@ def my_profile():
 @app.route('/testdb', methods=["POST", "GET"])
 def testdb():
     if request.method == "POST":
-        # create random instance of User class
-        new_user = User(full_name="karl", user_name="karldudley", email="karl@example.com", password="test123")
+        # create random instance of Student class
+        new_student = Student(full_name="karl", user_name="karldudley", email="karl@example.com", password="test123")
 
         # store random string and url in db
         try:
-            db.session.add(new_user)
+            db.session.add(new_student)
             db.session.commit()
             return "Correctly added to db"
-            # user_table = User.query.all()
-            # return user_table
+            # student_table = Student.query.all()
+            # return student_table
         except:
             return "There was an error adding to the db"
     else:
-        users = User.query.order_by(User.created_at)
-        print(users[0].full_name)
+        students = Student.query.order_by(Student.created_at)
+        for x in students:
+            print(x.full_name, x .user_name, x.email, x.password)
         return "hey"
     
 
