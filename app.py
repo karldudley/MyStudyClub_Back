a8ea6e2e4eb852f6e5e5b6b733955f87f7feb914
karl dudley <karldudley@gmail.com>
2022-10-27 22:44:25 +0100

    add join table

--- app.py
+++ app.py
@@ -6,11 +6,22 @@ app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student.db' # flask sqlite db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://knonbgucbylgdb:91620f58ea09dd7d85b9d24e4b7a26372ea08ee1bede0e8e3bbb3bfc139ec5fc@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d57frogopfmo03' # heroku postgres db
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialise the db
 db = SQLAlchemy(app)
-# heroku pg:psql
+
+# USEFUL COMMANDS
+# heroku pg:psql to go into sql shell
+# heroku pg:reset DATABASE to reset db
+# heroku restart to reset server
+
+
+# join table for many to many
+student_club = db.Table('student_club',
+                    db.Column('student_id', db.Integer, db.ForeignKey('student.id')),
+                    db.Column('club_id', db.Integer, db.ForeignKey('club.id'))
+                    )
 
 # Create Student model
 class Student(db.Model):
@@ -21,10 +32,25 @@ class Student(db.Model):
     email = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(20), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    clubs = db.relationship('Club', secondary=student_club, backref='students')
 
     #create a function to return a string when we add something
     def __repr__(self):
-        return '<Student %r>' % self.id
+        return '<Student %r>' % self.full_name
+
+# Create Club model
+class Club(db.Model):
+    __tablename__ = 'club'
+    id = db.Column(db.Integer, primary_key=True)
+    club_name = db.Column(db.String(50), nullable=False)
+    club_code = db.Column(db.String(20), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    # set_id foreign key
+    # message_id foreign key
+
+    #create a function to return a string when we add something
+    def __repr__(self):
+        return '<Club %r>' % self.club_name
 
 @app.route('/')
 def index():
@@ -44,10 +70,13 @@ def testdb():
     if request.method == "POST":
         # create random instance of Student class
         new_student = Student(full_name="karl", user_name="karldudley", email="karl@example.com", password="test123")
+        # create random instance of Club class
+        new_club = Club(club_name="science", club_code="999")
 
         # store random string and url in db
         try:
             db.session.add(new_student)
+            db.session.add(new_club)
             db.session.commit()
             return "Correctly added to db"
             # student_table = Student.query.all()
@@ -56,8 +85,11 @@ def testdb():
             return "There was an error adding to the db"
     else:
         students = Student.query.order_by(Student.created_at)
+        clubs = Club.query.order_by(Club.created_at)
         for x in students:
             print(x.full_name, x .user_name, x.email, x.password)
+        for x in clubs:
+            print(x.club_name, x .club_code)
         return "hey"
     
 
