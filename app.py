2878bcac95e757db8446a399ee52ba7978572d35
Camila Pessoa <caape13@hotmail.com>
2022-10-29 10:34:37 +0100

    implement Auth&auth

--- app.py
+++ app.py
@@ -14,14 +14,15 @@ cors = CORS(app)
 
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student.db' # flask sqlite db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://knonbgucbylgdb:91620f58ea09dd7d85b9d24e4b7a26372ea08ee1bede0e8e3bbb3bfc139ec5fc@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d57frogopfmo03' # heroku postgres db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://knonbgucbylgdb:91620f58ea09dd7d85b9d24e4b7a26372ea08ee1bede0e8e3bbb3bfc139ec5fc@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d57frogopfmo03'  # heroku postgres db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app) # Initialise the db
+db = SQLAlchemy(app)  # Initialise the db
 ma = Marshmallow(app)
 
-app.config['JWT_SECRET_KEY'] = 'Remember to change me' #Config JWT secret key
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=3) #specifies token lifespan
+app.config['JWT_SECRET_KEY'] = 'Remember to change me'  # Config JWT secret key
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(
+    hours=3)  # specifies token lifespan
 jwt = JWTManager(app)
 
 conn = psycopg2.connect(database="d57frogopfmo03",
@@ -41,17 +42,24 @@ cursor = conn.cursor()
 
 # join table for many to many
 student_club = db.Table('student_club',
-                    db.Column('student_id', db.Integer, db.ForeignKey('student.id')),
-                    db.Column('club_id', db.Integer, db.ForeignKey('club.id'))
-                    )
+                        db.Column('student_id', db.Integer,
+                                  db.ForeignKey('student.id')),
+                        db.Column('club_id', db.Integer,
+                                  db.ForeignKey('club.id'))
+                        )
+
 
 class StudentClubSchema(ma.Schema):
     class Meta:
         fields = ('student_id', 'students.name', 'club_id', 'clubs.name')
+
+
 student_club_schema = StudentClubSchema()
 students_clubs_schema = StudentClubSchema(many=True)
 
 # Create Student model
+
+
 class Student(db.Model):
     __tablename__ = 'student'
     id = db.Column(db.Integer, primary_key=True)
@@ -62,17 +70,23 @@ class Student(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     clubs = db.relationship('Club', secondary=student_club, backref='students')
 
-    #create a function to return a string when we add something
+    # create a function to return a string when we add something
     def __repr__(self):
         return '<Student %r>' % self.id
 
+
 class StudentSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'full_name', 'user_name', 'email', 'password', 'created_at')
+        fields = ('id', 'full_name', 'user_name',
+                  'email', 'password', 'created_at')
+
+
 student_schema = StudentSchema()
 students_schema = StudentSchema(many=True)
 
 # Create Club model
+
+
 class Club(db.Model):
     __tablename__ = 'club'
     id = db.Column(db.Integer, primary_key=True)
@@ -80,19 +94,25 @@ class Club(db.Model):
     club_code = db.Column(db.String(20), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     sets = db.relationship('Set', backref='club')  # setup foreign key for sets
-    messages = db.relationship('Message', backref='club')  # setup foreign key for messages
+    # setup foreign key for messages
+    messages = db.relationship('Message', backref='club')
 
-    #create a function to return a string when we add something
+    # create a function to return a string when we add something
     def __repr__(self):
         return '<Club %r>' % self.club_name
+
 
 class ClubSchema(ma.Schema):
     class Meta:
         fields = ('id', 'club_name', 'club_code', 'created_at')
+
+
 club_schema = ClubSchema()
 clubs_schema = ClubSchema(many=True)
 
 # Create Set model
+
+
 class Set(db.Model):
     __tablename__ = 'set'
     id = db.Column(db.Integer, primary_key=True)
@@ -100,60 +120,82 @@ class Set(db.Model):
     private = db.Column(db.Boolean, nullable=False)
     likes = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    club_id = db.Column(db.Integer, db.ForeignKey('club.id')) # link set to club
-    flashcards = db.relationship('Flashcard', backref='set')  # setup foreign key for flashcards
+    club_id = db.Column(db.Integer, db.ForeignKey(
+        'club.id'))  # link set to club
+    # setup foreign key for flashcards
+    flashcards = db.relationship('Flashcard', backref='set')
 
-    #create a function to return a string when we add something
+    # create a function to return a string when we add something
     def __repr__(self):
         return '<Set %r>' % self.set_name
 
+
 class SetSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'set_name', 'private', 'likes', 'created_at', 'club_id', 'club.club_name')
+        fields = ('id', 'set_name', 'private', 'likes',
+                  'created_at', 'club_id', 'club.club_name')
+
+
 set_schema = SetSchema()
 sets_schema = SetSchema(many=True)
 
 # Create Flashcard model
+
+
 class Flashcard(db.Model):
     __tablename__ = 'flashcard'
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(255), nullable=False)
     answer = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    set_id = db.Column(db.Integer, db.ForeignKey('set.id')) # link flashcard to set
+    set_id = db.Column(db.Integer, db.ForeignKey(
+        'set.id'))  # link flashcard to set
 
-    #create a function to return a string when we add something
+    # create a function to return a string when we add something
     def __repr__(self):
         return '<Flashcard %r>' % self.question[:20]
 
+
 class FlashcardSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'question', 'answer', 'created_at', 'set_id', 'set.set_name')
+        fields = ('id', 'question', 'answer',
+                  'created_at', 'set_id', 'set.set_name')
+
+
 flashcard_schema = FlashcardSchema()
 flashcards_schema = FlashcardSchema(many=True)
 
 # Create Message model
+
+
 class Message(db.Model):
     __tablename__ = 'message'
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, nullable=False) # link this to User? // change to student_id
-    club_id = db.Column(db.Integer, db.ForeignKey('club.id')) # link message to club
+    # link this to User? // change to student_id
+    user_id = db.Column(db.Integer, nullable=False)
+    club_id = db.Column(db.Integer, db.ForeignKey(
+        'club.id'))  # link message to club
 
-    #create a function to return a string when we add something
+    # create a function to return a string when we add something
     def __repr__(self):
         return '<Flashcard %r>' % self.content[:20]
+
 
 class MessageSchema(ma.Schema):
     class Meta:
         fields = ('id', 'content', 'created_at', 'club_id', 'club.club_name')
+
+
 message_schema = MessageSchema()
 messages_schema = MessageSchema(many=True)
+
 
 @app.route('/')
 def index():
     return "<h1 style=\"color:#D81159;\">Welcome to the myStudyClub server</h1><h2>The following GET endpoints currently exist:</h2><h3>/students</h3><h3>/students/:id</h3><h3>/clubs</h3><h3>/clubs/:id</h3><h3>/studentclubs</h3><h3>/studentclubs/:id</h3><h3>/sets</h3><h3>/sets/:id</h3><h3>/messages</h3><h3>/messages/:id</h3><h3>/flashcards</h3><h3>/flashcards/:set_id</h3>"
+
 
 @app.route('/studentclubs')
 def studentclubs():
@@ -161,11 +203,13 @@ def studentclubs():
     rows = cursor.fetchall()
     return jsonify(rows), 200
 
+
 @app.route('/studentclubs/<id>')
 def studentclub(id):
     cursor.execute(f"SELECT * FROM student_club WHERE student_id={id};")
     rows = cursor.fetchall()
     return rows, 200
+
 
 @app.route('/students')
 def students():
@@ -173,11 +217,13 @@ def students():
     res = students_schema.dump(data)
     return jsonify(res), 200
 
+
 @app.route('/students/<id>')
 def student(id):
     data = Student.query.get(id)
     res = student_schema.dump(data)
     return res, 200
+
 
 @app.route('/clubs')
 def clubs():
@@ -185,11 +231,13 @@ def clubs():
     res = clubs_schema.dump(data)
     return jsonify(res), 200
 
+
 @app.route('/clubs/<id>')
 def club(id):
     data = Club.query.get(id)
     res = club_schema.dump(data)
     return res, 200
+
 
 @app.route('/sets')
 def sets():
@@ -197,11 +245,13 @@ def sets():
     res = sets_schema.dump(data)
     return jsonify(res), 200
 
+
 @app.route('/sets/<id>')
 def set(id):
     data = Set.query.get(id)
     res = set_schema.dump(data)
     return res, 200
+
 
 @app.route('/flashcards')
 def flashcards():
@@ -215,11 +265,13 @@ def flashcards():
 #     res = flashcard_schema.dump(data)
 #     return res, 200
 
+
 @app.route('/flashcards/<set_id>')
 def flashcard_set(set_id):
     data = Flashcard.query.filter_by(set_id=set_id)
     res = flashcards_schema.dump(data)
     return jsonify(res), 200
+
 
 @app.route('/messages')
 def messages():
@@ -227,62 +279,72 @@ def messages():
     res = messages_schema.dump(data)
     return jsonify(res), 200
 
+
 @app.route('/messages/<id>')
 def message(id):
     data = Message.query.get(id)
     res = message_schema.dump(data)
     return res, 200
 
-#The function below is to guarantee token won't expire when user is logged in.The function takes as an argument, the response from the /profile API call.
-@app.after_request #Ensures that the refresh_expiring_jts runs after a request has been made to the protected PROFILE endpoint
+# The function below is to guarantee token won't expire when user is logged in.The function takes as an argument, the response from the /profile API call.
+
+
+@app.after_request  # Ensures that the refresh_expiring_jts runs after a request has been made to the protected PROFILE endpoint
 def refresh_expiring_jwts(response):
-	try:
-		exp_timestamp = get_jwt()["exp"] #current timestamp for user
-		now = datetime.now(timezone.utc)
-		target_timestamp = datetime.timestamp(now + timedelta(minutes=30)) #30 minutes away
-		if target_timestamp > exp_timestamp:
-			access_token = create_access_token(identity=get_jwt_identity)
-			data = response.get_json()
-			if type(data) is dict:
-				data["access_token"] = access_token
-				response.data=json.dumps(data)
-		return response
-	except (RuntimeError, KeyError):
-		# Case where there is not a valid JWT. Just return the original response
-		return response
+    try:
+        exp_timestamp = get_jwt()["exp"]  # current timestamp for user
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(
+            now + timedelta(minutes=30))  # 30 minutes away
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity)
+            data = response.get_json()
+            if type(data) is dict:
+                data["access_token"] = access_token
+                response.data = json.dumps(data)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original response
+        return response
+
 
 @app.route('/token', methods=['POST'])
 def create_token():
-	email = request.json.get('email', None)
-	password = request.json.get('password', None)
-	#Change this later => compare with database user details
-	if email != 'test' or password != 'test':
-		return{'message': 'Wrong email or password'}, 401 #unauthorized Error
-	#create access token for particular email if login is confirmed
-	access_token = create_access_token(identity=email)
-	response = {'access_token': access_token}
-	return response, 200
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    # Change this later => compare with database user details
+    if email != 'test' or password != 'test':
+        # unauthorized Error
+        return {'message': 'Wrong email or password'}, 401
+    # create access token for particular email if login is confirmed
+    access_token = create_access_token(identity=email)
+    response = {'access_token': access_token}
+    return response, 200
+
 
 @app.route('/logout', methods=['POST'])
 def logout():
-	response = jsonify({"message": "logout successful"})
-	unset_jwt_cookies(response)
-	return response, 200
+    response = jsonify({"message": "logout successful"})
+    unset_jwt_cookies(response)
+    return response, 200
+
 
 @app.route('/profile')
-@jwt_required() #prevent unauthenticated users from making requests to the API
+@jwt_required()  # prevent unauthenticated users from making requests to the API
 def my_profile():
     response_body = {
         "name": "Karlos",
-        "about" :"Hello! I'm a full stack developer that loves Python and React"
+        "about": "Hello! I'm a full stack developer that loves Python and React"
     }
     return response_body
+
 
 @app.route('/testdb', methods=["POST", "GET"])
 def testdb():
     if request.method == "POST":
         # create random instance of Student class
-        new_student = Student(full_name="karl", user_name="karldudley", email="karl@example.com", password="test123")
+        new_student = Student(full_name="karl", user_name="karldudley",
+                              email="karl@example.com", password="test123")
         # create random instance of Club class
         new_club = Club(club_name="science", club_code="999")
 
@@ -314,7 +376,7 @@ def testdb():
         for x in flashcards:
             print(x.question, x.answer)
         return "hey"
-    
+
 
 if __name__ == '__main__':
-    app.run(debug = True)   # pragma: no cover
+    app.run(debug=True)   # pragma: no cover
